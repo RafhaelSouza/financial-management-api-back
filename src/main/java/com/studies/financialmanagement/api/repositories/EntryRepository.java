f9d9596981e76625f649aa93ade74b9701e5b6a8
Rafhael <rafhael.dev@gmail.com>
2021-11-25 07:31:22 -0300

    Searching due entries

--- src/main/java/com/studies/financialmanagement/api/repositories/EntryRepository.java
+++ src/main/java/com/studies/financialmanagement/api/repositories/EntryRepository.java
@@ -5,6 +5,12 @@ import com.studies.financialmanagement.api.repositories.entry.EntryRepositoryQue
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long>, EntryRepositoryQuery {
+
+    List<Entry> findByDueDateLessThanEqualAndPaymentDueIsNull(LocalDate data);
+
 }
--- src/main/java/com/studies/financialmanagement/api/repositories/UsersRepository.java
+++ src/main/java/com/studies/financialmanagement/api/repositories/UsersRepository.java
@@ -4,11 +4,14 @@ import com.studies.financialmanagement.api.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
+
+    List<Users> findByPermissionsDescription(String permissionDescription);
 
 }
